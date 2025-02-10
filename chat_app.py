56c52d1c9ac56051b75e0f139eb00bed73dba27b
Yates <Matt.Yates@afgroupazure.com>
2025-02-10 14:11:33 -0500

    created simple chat ui

--- chat_app.py
+++ chat_app.py
@@ -2,5 +2,17 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+def get_chat_response(input):
+    return "Hi!"
+
+@app.route('/')
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/chat_get", methods=["GET", "POST"])
+def chat_get():
+    msg = request.form["msg"]
+    return get_chat_response(msg)
+
 if __name__ == '__main__': 
     app.run(debug=False) 
---
+++ static/chat.js
@@ -0,0 +1,49 @@
+// Main Chat
+
+// Basic Chat Functionality
+$(document).ready(function() {
+    $("#chatMessageArea").on("submit", function(event) {
+        var rawText = $("#text").val();
+
+        // https://getbootstrap.com/docs/4.0/utilities/flex/
+        // d-flex creates a flexbox container which can be modified with additional shorthand properties
+        // https://getbootstrap.com/docs/4.0/utilities/flex/#justify-content
+        // justify-content-end is bootstrap shorthand to change the alignment of the flex item to the right side
+        // https://getbootstrap.com/docs/4.1/utilities/spacing/
+        // mb-4 is bootstrap shorhand for a margin-bottom of $spacer * 1.5
+        var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + rawText + '<span class="msg_time_send"></span></div><div class="img_cont_msg"><img src="/static/user.png" class="rounded-circle user_img_msg"></div></div>';
+        
+        $("#text").val("");
+        // The append() method inserts specified content at the end of the selected elements.
+        // i.e. within the div with ID=chatMessageForm, the userHtml content will be added AS THE LAST element within the div chatMessageForm
+        //      so if div chatMessageForm has 4 child divs, then this would add a 5th child div at the end of the 4 child divs  
+        //      see https://www.w3schools.com/jquery/html_append.asp for demo of this
+        $("#chatMessageForm").append(userHtml);
+
+        // Thinking...
+        var thinking = '<div id="fullSpinner"><div class="spinner-border text-light" role="status"><span class="visually-hidden">Thinking...</span></div><span style="color:white;padding:20px;">Thinking...</span></div>'
+        $("#chatMessageForm").append(thinking);
+        // scroll down
+        jQuery(document).find("#chatMessageForm").scrollTop(function(){ return this.scrollHeight });
+
+        $.ajax({
+            data: {
+                msg: rawText
+            },
+            type: "POST",
+            url: "/chat_get",
+        }).done(function(data) {
+            // removing thinking
+            $("#fullSpinner").remove();
+            // add response
+            var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="/static/robot-assistant.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time"></span></div></div>';
+            $("#chatMessageForm").append($.parseHTML(botHtml));
+            // scroll down
+            jQuery(document).find("#chatMessageForm").scrollTop(function(){ return this.scrollHeight });
+        });
+        event.preventDefault();
+
+    });
+});
+
+
