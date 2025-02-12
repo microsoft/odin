02554f80a2bd1c38705bd3d224aac3c299d3cbc7
Reiss <Steven.Reiss@afgroupazure.com>
2025-02-12 13:07:47 -0500

    created requirements.txt file

--- src/llms/llms.py
+++ src/llms/llms.py
@@ -1,7 +1,5 @@
 from langchain_openai import AzureChatOpenAI
 
-# from dotenv import load_dotenv
-# load_dotenv()
 llm_4o = AzureChatOpenAI(
     azure_deployment="DS-Claims-RnD-gpt-4o",
     api_version='2024-08-01-preview',
