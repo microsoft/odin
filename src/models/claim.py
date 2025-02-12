35e47f0e2def6de122a44a755ca462ccdd51386d
MJ Schanne <mjschanne@microsoft.com>
2025-02-12 16:30:36 -0500

    Add claim.py

---
+++ src/models/claim.py
@@ -0,0 +1,6 @@
+class Claim:
+    def __init__(self, claim_id, description, amount, status):
+        self.claim_id = claim_id
+        self.description = description
+        self.amount = amount
+        self.status = status
