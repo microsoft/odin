31cfc7afa5eb050b5765ebff2b006754f3b05c2b
MJ Schanne <mjschanne@microsoft.com>
2025-02-13 09:02:51 -0500

    Integrate API changes with the UI

--- src/static/chat.js
+++ src/static/chat.js
@@ -1,8 +1,8 @@
 // Main Chat
 
 // Basic Chat Functionality
-$(document).ready(function() {
-    $("#chatMessageArea").on("submit", function(event) {
+$(document).ready(function () {
+    $("#chatMessageArea").on("submit", function (event) {
         var rawText = $("#text").val();
 
         // https://getbootstrap.com/docs/4.0/utilities/flex/
@@ -12,7 +12,7 @@ $(document).ready(function() {
         // https://getbootstrap.com/docs/4.1/utilities/spacing/
         // mb-4 is bootstrap shorhand for a margin-bottom of $spacer * 1.5
         var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + rawText + '<span class="msg_time_send"></span></div><div class="img_cont_msg"><img src="/static/user.png" class="rounded-circle user_img_msg"></div></div>';
-        
+
         $("#text").val("");
         // The append() method inserts specified content at the end of the selected elements.
         // i.e. within the div with ID=chatMessageForm, the userHtml content will be added AS THE LAST element within the div chatMessageForm
@@ -24,26 +24,41 @@ $(document).ready(function() {
         var thinking = '<div id="fullSpinner"><div class="spinner-border text-light" role="status"><span class="visually-hidden">Thinking...</span></div><span style="color:white;padding:20px;">Thinking...</span></div>'
         $("#chatMessageForm").append(thinking);
         // scroll down
-        jQuery(document).find("#chatMessageForm").scrollTop(function(){ return this.scrollHeight });
+        jQuery(document).find("#chatMessageForm").scrollTop(function () { return this.scrollHeight });
+
+        let conversationId = null;
+        let claimId = $("#claimsDropdown option:selected").val();
+         let converseUrl = conversationId
+            ? `/claims/${claimId}/conversations/${conversationId}`
+            : `/claims/${claimId}/conversations`;
 
         $.ajax({
-            data: {
-                msg: rawText
-            },
             type: "POST",
-            url: "/chat_get",
-        }).done(function(data) {
+            url: converseUrl,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            dataType: "json",
+            data: JSON.stringify({
+                conversation_id: conversationId,
+                claim_id: claimId,
+                messages: [
+                    {
+                        content: rawText,
+                        date: new Date().toISOString(),
+                        role: "user"
+                    }
+                ]
+            })
+        }).done(function (data) {
             // removing thinking
             $("#fullSpinner").remove();
             // add response
             var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="/static/robot-assistant.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time"></span></div></div>';
             $("#chatMessageForm").append($.parseHTML(botHtml));
             // scroll down
-            jQuery(document).find("#chatMessageForm").scrollTop(function(){ return this.scrollHeight });
+            jQuery(document).find("#chatMessageForm").scrollTop(function () { return this.scrollHeight });
         });
         event.preventDefault();
-
     });
 });
-
-
