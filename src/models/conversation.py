297302b600a2d9fca592fd83aaa7ca1dbfa21655
MJ Schanne <mjschanne@microsoft.com>
2025-02-13 13:19:08 -0500

    Add typing to Conversation.py

--- src/models/conversation.py
+++ src/models/conversation.py
@@ -1,5 +1,8 @@
+from typing import Dict
+
+
 class Conversation:
-    def __init__(self, conversation_id, claim_id, messages):
+    def __init__(self, conversation_id, claim_id, messages: list[Dict[str, str]]):
         self.conversation_id = conversation_id
         self.claim_id = claim_id
         # messages should be https://python.langchain.com/api_reference/core/messages/langchain_core.messages.base.BaseMessage.html#basemessage
