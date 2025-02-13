60f3e6af75b4aae7b2a6aeace2358f672813c39a
MJ Schanne <mjschanne@microsoft.com>
2025-02-13 09:00:05 -0500

    Update id to conversation_id for conversation.py

--- src/models/conversation.py
+++ src/models/conversation.py
@@ -1,6 +1,6 @@
 class Conversation:
-    def __init__(self, id, claim_id, messages):
-        self.id = id
+    def __init__(self, conversation_id, claim_id, messages):
+        self.conversation_id = conversation_id
         self.claim_id = claim_id
         # messages should be https://python.langchain.com/api_reference/core/messages/langchain_core.messages.base.BaseMessage.html#basemessage
         self.messages = messages
