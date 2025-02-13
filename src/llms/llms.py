df5df693ec0859e0e96271fe3d27802bca791ac8
MJ Schanne <mjschanne@microsoft.com>
2025-02-13 13:19:59 -0500

    Add managed identity support to llms.py

--- src/llms/llms.py
+++ src/llms/llms.py
@@ -1,8 +1,28 @@
 from langchain_openai import AzureChatOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from config import config
 
-llm_4o = AzureChatOpenAI(
-    azure_deployment="DS-Claims-RnD-gpt-4o",
-    api_version='2024-08-01-preview',
-    temperature=0,
-    model = 'gpt-4o'
-)
+if config.azure_openai_api_key is None or config.azure_openai_api_key == "":
+    if config.tenant_id is None or config.tenant_id == "":
+        credentials = DefaultAzureCredential()
+    else:
+        credentials = DefaultAzureCredential(tenant_id=config.tenant_id)
+
+    token_provider = get_bearer_token_provider(
+        credentials, "https://cognitiveservices.azure.com/.default"
+    )
+
+    llm_4o = AzureChatOpenAI(
+        azure_deployment=config.azure_openai_deployment,
+        api_version=config.azure_openai_version,
+        azure_ad_token_provider=token_provider,
+        # temperature=0,
+    )
+else:
+    llm_4o = AzureChatOpenAI(
+        azure_deployment=config.azure_openai_deployment,
+        api_version=config.azure_openai_version,
+        # temperature=0,
+    )
+
+__all__ = [llm_4o]
