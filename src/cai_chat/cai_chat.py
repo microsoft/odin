8321a97ab00781df370669fd1d6aebf0637622c1
MJ Schanne <mjschanne@microsoft.com>
2025-02-26 01:48:55 -0500

    Format cai_chat.py

--- src/cai_chat/cai_chat.py
+++ src/cai_chat/cai_chat.py
@@ -4,9 +4,10 @@ from opentelemetry import trace
 
 tracer = trace.get_tracer(__name__)
 
+
 def run_agent(query: str, claimnumber: str, chat_history: List[Dict[str, Any]] = []):
     #  https://docs.smith.langchain.com/observability/how_to_guides/trace_with_opentelemetry
-     with tracer.start_as_current_span("call_open_ai") as span:
+    with tracer.start_as_current_span("call_open_ai") as span:
         # span.set_attribute("langsmith.span.kind", "LLM")
         # span.set_attribute("langsmith.metadata.user_id", "user_123")
         span.set_attribute("gen_ai.system", "az.ai.inference")
@@ -17,10 +18,12 @@ def run_agent(query: str, claimnumber: str, chat_history: List[Dict[str, Any]] =
         #     span.set_attribute(f"gen_ai.prompt.{i}.role", str(message["role"]))
 
         result = app.invoke(
-        {'question': query,
-         'chat_history': chat_history,
-         'documents': None,
-         'claimnumber': claimnumber}
+            {
+                "question": query,
+                "chat_history": chat_history,
+                "documents": None,
+                "claimnumber": claimnumber,
+            }
         )
 
         # span.set_attribute("gen_ai.response.model", completion.model)
@@ -29,5 +32,5 @@ def run_agent(query: str, claimnumber: str, chat_history: List[Dict[str, Any]] =
         # span.set_attribute("gen_ai.usage.prompt_tokens", completion.usage.prompt_tokens)
         # span.set_attribute("gen_ai.usage.completion_tokens", completion.usage.completion_tokens)
         # span.set_attribute("gen_ai.usage.total_tokens", completion.usage.total_tokens)
-    
+
         return result
